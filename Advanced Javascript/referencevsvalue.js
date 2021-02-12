8f7c32c534992ce257d41ad9a12220ebb8a8b381
Julia Mathias <juliamss@gmail.com>
2021-02-12 16:11:45 -0300

    Add deep clone example

--- Advanced Javascript/referencevsvalue.js
+++ Advanced Javascript/referencevsvalue.js
@@ -13,8 +13,14 @@ d.push(56454545)
 console.log(d)
 
 let obj = { a: 'a', b: 'b', c: 'c' };
+
+// shallow clone
 let clone = Object.assign({}, obj);
 let clone2 = {...obj} // new feature
+
+// deep clone
+let superClone = JSON.parse(JSON.stringfy(obj))
+
 
 obj.c = 5;
 console.log(clone)
