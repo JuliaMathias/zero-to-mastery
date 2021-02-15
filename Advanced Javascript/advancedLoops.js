1e49b5c37cc986cca017075a5a75cef424333d52
Julia Mathias <juliamss@gmail.com>
2021-02-15 19:43:06 -0300

    Add advanced loop examples

---
+++ Advanced Javascript/advancedLoops.js
@@ -0,0 +1,32 @@
+const basket = ['apples', 'oranges', 'grapes'];
+const detailedBasket = {
+  apples: 5,
+  oranges: 10,
+  grapes: 1000
+}
+
+// old way
+
+for (let i = 0 ; i < basket.length; i++) {
+  console.log(basket[i]);
+}
+
+
+// ES5 way
+
+basket.forEach(item => {
+  console.log(item);
+})
+
+// for of
+// iterating - arrays, strings
+
+for (item of basket) {
+  console.log(item);
+}
+
+// for in - objects
+
+for (item in destailedBasket) {
+  console.log(item);
+}
