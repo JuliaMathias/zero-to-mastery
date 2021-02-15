db498583c0137e8dbcc0ec994a12cdc3597b02e4
Julia Mathias <juliamss@gmail.com>
2021-02-15 16:43:51 -0300

    Add fromEntries and try/catch methods examples

--- Advanced Javascript/ES10.js
+++ Advanced Javascript/ES10.js
@@ -1,4 +1,4 @@
-// flat method
+// flat + flatmap methods
 
 const array = [1, [2, 3], [4,5]]
 
@@ -14,8 +14,28 @@ entries.flat()
 const rollcallEntries = entries.flatMap(entry => 'I am ' + entry)
 rollcallEntries
 
+// trim methods
+
 const userEmail = '         eddy@gmail.com'
 const userEmail2 = 'johnny@gmail.com         '
 
 console.log(userEmail.trimStart())
 console.log(userEmail2.trimEnd())
+
+// fromEntries
+
+userProfiles = [['commanderTom', 23], ['derekZlander', 40], ['hanselOrg', 28]]
+
+Object.fromEntries(userProfiles)
+
+
+// try, catch, block
+
+try {
+
+  bob + 'hi'
+
+} catch {
+
+  console.log('you messed up')
+}
