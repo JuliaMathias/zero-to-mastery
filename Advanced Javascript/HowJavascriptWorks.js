55b67342cc7154187929de121717a794409bbc20
Julia Mathias <juliamss@gmail.com>
2021-02-20 19:18:18 -0300

    Add call stack, recursion and async examples

---
+++ Advanced Javascript/HowJavascriptWorks.js
@@ -0,0 +1,30 @@
+const a = 1;
+const b = 10;
+const c = 100;
+
+// call stack
+console.log('1');
+console.log('2');
+console.log('3');
+
+const one = () => {
+  const two = () => {
+    console.log('4');
+  }
+
+  two();
+}
+
+// Recursion - example of how to cause a stack oveflow
+
+const foo = () => {
+  foo();
+}
+
+foo()
+
+//Async
+
+setTimeout(() => {
+  console.log('2');
+}, 2000)
